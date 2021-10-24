896663facfb83a897a0cbf27717d9ed6627241e7
unicage <ulugbekghostbatory@gmail.com>
2021-10-24 17:06:10 +0500

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DeleteView):
+    model = Post
+    template_name = 'post_detail.html'
+# Create your views here.
