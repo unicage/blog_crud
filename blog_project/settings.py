14ff8fc31320517bb4e6bb4aaba25ce4df025448
unicage <ulugbekghostbatory@gmail.com>
2021-10-26 23:33:49 +0500

    heroku again

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
