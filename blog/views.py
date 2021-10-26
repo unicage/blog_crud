495e9933eb01879544ce39039abc24c31cdee70f
unicage <ulugbekghostbatory@gmail.com>
2021-10-26 23:11:51 +0500

    some Heroku again

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Post
 
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    context_object_name = 'all_posts_list'
 
-class BlogDetailView(DeleteView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
 # Create your views here.
